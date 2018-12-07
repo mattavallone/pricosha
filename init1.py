dffb2fc4c712db6804fe5dcfd2ed60ce826d34a0
Allieshaber@gmail.com <allieshaber@gmail.com>
2018-12-07 13:17:16 -0500

    Merge branch 'master' of /Users/alfredhaber/PycharmProjects/pricosha with conflicts.

--- init1.py
+++ init1.py
@@ -95,27 +95,7 @@ def home():
     cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
-
-    cursor = conn.cursor()
-    fgnames = 'SELECT fg_name FROM friendgroup WHERE owner_email = %s'
-    cursor.execute(fgnames, email)  # retrieving friend groups existing in database
-    fg = cursor.fetchall()  # returns tuples of possible friend group names that exist in DB
-    cursor.close()
-
-    cursor = conn.cursor()
-    createFGview = 'CREATE VIEW FG AS (SELECT fg_name FROM belong WHERE belong.email = %s); '
-    cursor.execute(createFGview, email)
-    conn.commit()
-    locdata = 'SELECT location, email_post AS Email FROM contentItem WHERE location IS NOT NULL AND ' \
-              'contentItem.email_post IN (SELECT DISTINCT email FROM belong NATURAL JOIN FG WHERE email != %s)AND ' \
-              'location IN (SELECT DISTINCT location FROM contentItem WHERE email_post = %s); '
-    cursor.execute(locdata, (email,email))
-    loc = cursor.fetchall()
-    dropFGview = 'DROP VIEW FG;'
-    cursor.execute(dropFGview)
-    conn.commit()
-    cursor.close()
-    return render_template('home.html', username=email, posts=data, fg=fg, locdata=loc)
+    return render_template('home.html', username=email, posts=data)
 
 
 @app.route('/logout')
@@ -141,35 +121,39 @@ def post():
     if is_private == '1':
         is_public = '0'
 
-        friend_group = request.form.getlist('friend_group')
-
-        cursor = conn.cursor()
-        query = 'INSERT INTO contentItem (email_post, post_time, file_path, item_name, location, is_pub) VALUES(' \
-                '%s, %s, %s, %s, %s, %s) '
-        cursor.execute(query, (email, date, file_path, item_name, location, is_public))
-        conn.commit()
+        friend_group = request.form['friend_group']
+        checkFGname = 'SELECT fg_name FROM friendgroup WHERE owner_email = %s AND fg_name = %s'
+        cursor.execute(checkFGname, (email, friend_group))  # checking to see if friend group exists in database
+        fgname = cursor.fetchone()  # returns tuples of possible friend group names that exist in DB
         cursor.close()
 
-        cursor = conn.cursor()
-        getItemID = 'SELECT item_id FROM contentItem WHERE item_name = %s'
-        cursor.execute(getItemID, item_name)  # retrieve correct item_id from database to use in next query
-        item_id = cursor.fetchone()
-        item_id = dict(item_id)  # need to turn tuple into dictionary for easy data access
-        cursor.close()
-
-        cursor = conn.cursor()  # updating share preferences for friend group
-        addItem2FG = 'INSERT INTO share(owner_email, fg_name, item_id) VALUES(%s, %s, %s)'
-        for fg_name in friend_group:
-            cursor.execute(addItem2FG, (email, fg_name, int(item_id['item_id'])))
+        if fgname:  # if friend group name is valid for specific user, allow group to see content item
+            cursor = conn.cursor()
+            query = 'INSERT INTO contentItem (email_post, post_time, file_path, item_name, location, is_pub) VALUES(' \
+                    '%s, %s, %s, %s, %s, %s) '
+            cursor.execute(query, (email, date, file_path, item_name, location, is_public))
             conn.commit()
+            cursor.close()
+
+            cursor = conn.cursor()
+            getItemID = 'SELECT item_id FROM contentItem WHERE item_name = %s'
+            cursor.execute(getItemID, item_name)  # retrieve correct item_id from database to use in next query
+            item_id = cursor.fetchone()
+            item_id = dict(item_id)  # need to turn tuple into dictionary for easy data access
+            cursor.close()
+
+            cursor = conn.cursor()
+            addItem2FG = 'INSERT INTO share(owner_email, fg_name, item_id) VALUES(%s, %s, %s)'
+            cursor.execute(addItem2FG, (email, friend_group, int(item_id['item_id'])))
+            # updating share preferences for friend group
 
     else:
         is_public = '1'
         query = 'INSERT INTO contentItem (email_post, post_time, file_path, item_name, location, is_pub) VALUES(%s, ' \
                 '%s, %s, %s, %s, %s) '
         cursor.execute(query, (email, date, file_path, item_name, location, is_public))
-        conn.commit()
 
+    conn.commit()
     cursor.close()
     return redirect(url_for('home'))
 
